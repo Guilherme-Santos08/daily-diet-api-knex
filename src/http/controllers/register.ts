a2e8ece074497f0b0cc3a659db3f79f1f9b83aee
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2024-04-28 15:12:30 -0300

    improve error handling

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,3 +1,4 @@
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -16,7 +17,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    throw err
   }
 
   return reply.status(201).send()
