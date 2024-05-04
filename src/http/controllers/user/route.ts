9f48599f85dbbc6ce4ccae58701c2dd9516578fd
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2024-05-04 17:48:50 -0300

    organizing controller

--- src/http/controllers/user/route.ts
+++ src/http/controllers/user/route.ts
@@ -1,6 +1,6 @@
-import { authenticate } from '@/http/controllers/authenticate'
-import { register } from '@/http/controllers/register'
 import { FastifyInstance } from 'fastify'
+import { register } from './register'
+import { authenticate } from './authenticate'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
